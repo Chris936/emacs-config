4f328e5b9c536fa794512ebe5451dea8d2469202
Christoffer Samuelsson <christoffer.samuelsson222@gmail.com>
2018-05-26 21:16:57 +0200

    Macro to grep recursively for a targeted word.

--- init.el
+++ init.el
@@ -17,6 +17,11 @@
 
 (use-package try
   :ensure t)
+
+;; grep macro - Recursivlely greps for a targeted word from 2 directories back.
+(fset 'grep-targeted-word
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217826 67108896 134217830 134217847 134217848 103 114 101 112 return 25 32 46 46 47 32 45 82 return] 0 "%d")) arg)))
+
 ;; Function to load files
 (defun load-if-exists (f)
   (if (file-readable-p f)
